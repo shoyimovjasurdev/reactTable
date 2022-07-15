900b136ec938cca663929cc9e13379a6cbd4f2b9
ShoyimovJasur <jasurdev8@gmail.com>
2022-07-16 02:15:18 +0500

    React tabel

--- src/App.js
+++ src/App.js
@@ -1,25 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state={
+      main: [],
+      name:"",
+      Last_name:"",
+      Position:"",
+      Contak:"",
+      currentIndex:null
+    }
+  }
+  
+  AddFun(){
+    let arre = this.state.main
+
+    arre.push({
+      name:this.state.name,
+      Last_name:this.state.Last_name,
+      Position:this.state.Position,
+      Contak:this.state.Contak
+    })
+
+    this.setState({
+      main:arre,
+      name:"",
+      Last_name:"",
+      Position:"",
+      Contak:""
+    })
+  }
+
+  Edite(){
+    let arre = this.state.main
+    
+    arre[this.state.currentIndex]={
+      name:this.state.name,
+      Last_name:this.state.Last_name,
+      Position:this.state.Position,
+      Contak:this.state.Contak
+    }
+
+    this.setState({
+      main:arre,
+      name:"",
+      Last_name:"",
+      Position:"",
+      Contak:""
+    })
+  }
+
+  deleteFun(index){
+    let arre = this.state.main
+    arre.splice(index,1)
+    this.setState({main:arre})
+  }
+
+  editeFun(index){
+    let arre = this.state.main
+    this.setState({name:arre[index].name,
+      Last_name:arre[index].Last_name,
+      Position:arre[index].Position,
+      Contak:arre[index].Contak,
+      currentIndex:index})
+  }
+  
+  render() {
+
+    return (
+      <div className="App container mt-5">
+        <div>
+          <input value={this.state.name} onChange={(val)=>{this.setState({name:val.target.value})}} placeholder="Name" className="me-3"/>
+          <input value={this.state.Last_name} onChange={(val)=>{this.setState({Last_name:val.target.value})}} placeholder="Last name"  className="me-3"/>
+          <input value={this.state.Position} onChange={(val)=>{this.setState({Position:val.target.value})}} placeholder="Position"  className="me-3"/>
+          <input type="number" value={this.state.Contak} onChange={(val)=>{this.setState({Contak:val.target.value})}} placeholder="Contak"  className="me-4"/>
+          <button onClick={()=>this.AddFun()} className="btn btn-primary ms-5" >Add</button>
+          <button onClick={()=>this.Edite()} className="btn btn-success ms-3" >Edite</button>
+        </div>
+      <table className="table mt-2">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Last name</th>
+            <th scope="col">Position</th>
+            <th scope="col">Contak</th>
+            <th scope="col">Edite / Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            this.state.main.map((i,index)=>{
+              return(
+                <tr key={index}>
+                  <th>{index+1}</th>
+                  <td>{i.name}</td>
+                  <td>{i.Last_name}</td>
+                  <td>{i.Position}</td>
+                  <td>{i.Contak}</td>
+                  <td>
+                    <button className="btn  btn-success me-2" onClick={()=>this.editeFun(index)}>Edite</button>
+                    <button className="btn btn-danger" onClick={()=>this.deleteFun(index)}>Delete</button>
+                  </td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
     </div>
-  );
+    )
+  }
 }
-
-export default App;
